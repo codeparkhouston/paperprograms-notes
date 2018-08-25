4a600ed93312c1098313711268dad4fdcc07747a
Shaun Williams <shaun.e.williams@gmail.com>
2018-08-25 13:31:20 -0500

    add stub for our drawing helpers

---
+++ canvas-sandbox/draw.js
@@ -0,0 +1,14 @@
+// Our simpler drawing API
+// based on Dynamicland illumination API
+
+// Draw
+const label = (ctx, text) => {};
+const text = (ctx, { text, x, y, fontSize, fontFace, color }) => {};
+const rectangle = (ctx, { x, y, width, height, stroke, fill }) => {};
+const circle = (ctx, { x, y, radius, stroke, fill }) => {};
+const line = (ctx, { from, to, radius, stroke, strokeWidth });
+
+// Animate
+const loop = (ctx, draw) => {
+  // pass time "t" to draw
+};
--- canvas-sandbox/worker.js
+++ canvas-sandbox/worker.js
@@ -1,7 +1,13 @@
+importScripts("draw.js"); // <-- our drawing helper
+
+console.log("e2d", e2d);
+
 onmessage = function(evt) {
   const canvas = evt.data.canvas;
 
   const ctx = canvas.getContext("2d");
   ctx.fillStyle = "#f00";
   ctx.fillRect(0, 0, 100, 100);
+
+  // TODO: test our drawing helpers here
 };
